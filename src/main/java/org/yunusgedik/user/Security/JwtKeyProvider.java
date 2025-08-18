da6b8ee28dc6ba958033a7db3f6a62bf0183fc78
Yunus Gedik <yunus-gedik@outlook.com>
2025-08-19 02:15:36 +0300

    feat: add public key and public key reader method

--- src/main/java/org/yunusgedik/user/Security/JwtKeyProvider.java
+++ src/main/java/org/yunusgedik/user/Security/JwtKeyProvider.java
@@ -1,19 +1,30 @@
 package org.yunusgedik.user.Security;
 
+import lombok.Getter;
 import org.springframework.stereotype.Component;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
+@Getter
 @Component
 public class JwtKeyProvider {
 
-    private final PrivateKey privateKey;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
 
     public JwtKeyProvider() throws Exception {
+        initPrivateKey();
+        initPublicKey();
+    }
+
+    private void initPrivateKey() throws Exception {
         String key = Files.readString(Paths.get("src/main/resources/keys/jwt_private.pem"))
             .replaceAll("-----\\w+ PRIVATE KEY-----", "")
             .replaceAll("\\s", "");
@@ -24,7 +35,15 @@ public class JwtKeyProvider {
         this.privateKey = kf.generatePrivate(spec);
     }
 
-    public PrivateKey getPrivateKey() {
-        return privateKey;
+    private void initPublicKey() throws Exception {
+        String key = Files.readString(Paths.get("src/main/resources/keys/jwt_public.pem"))
+            .replaceAll("-----\\w+ PUBLIC KEY-----", "")
+            .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(key);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        this.publicKey = kf.generatePublic(spec);
     }
+
 }
